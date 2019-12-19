e8899ef5f02f04bf694e8863f6984517b4bc8d7b
Harsh <harsh.26020@gmail.com>
2019-12-19 20:01:07 +0530

    INput and select language done

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ import time
 from selenium.webdriver.common.keys import Keys
 import requests
 from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import Select
+import os
 
 max_pages = 100
-# username = input("Enter your handle/email = ")
-# password = input("Enter password = ")
+username = input("Enter your handle/email = ")
+password = input("Enter password = ")
 # contest_id = input("Enter contest id = ")
 # problem_id = input("Enter problem id = ")
 contest_id = "903"
 problem_id = "A"
 
 replace = {'&quot;': '\"', '&gt;': '>', '&lt;': '<', '&amp;': '&', "&apos;": "'"}
+extension = {'GNU C++14':'cpp', 'GNU C++11':'cpp', 'GNU C++17':'cpp', 'Java 8':'java', 'Python 3': 'py', 'Python 2':'py'}
+languages = {'GNU C++14':'GNU G++14 6.4.0', 'GNU C++11':'GNU G++11 5.1.0', 'GNU C++17':'GNU G++17 7.3.0', 'Java 8':'Java 11.0.5', 'Python 3': 'Python 3.7.2', 'Python 2':'Python 2.7.15'}
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver",chrome_options=options)
 
-# options = webdriver.ChromeOptions()
-# driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver",chrome_options=options)
 def login(username, password):
 	driver.get('https://www.codeforces.com/enter')
 	# enter username
@@ -35,15 +39,28 @@ def parse(code):
 		code = code.replace(key, replace[key])
 	return code
 
+def runCode(language):
+	driver.get('https://codeforces.com/problemset/customtest')
+	driver.find_element_by_name("inputFile").send_keys(os.path.join(os.getcwd(),'./code.'+extension[language]))
+	select = Select(driver.find_element_by_name('programTypeId'))
+	select.select_by_visible_text(languages[language])
+	text_box = driver.find_elements_by_name("input")[0]
+	text_box.send_keys("sa")
+
 #fetches the code corresponding to the parameter url
 def getCode(submission_url):
 	source = requests.get(submission_url).text
 	soup = BeautifulSoup(source, "lxml")
 	# find language
 	language = soup.findAll('td')[3].text[6:-6]
+	if language not in extension.keys():
+		return
 	code = soup.findAll('pre')[0].text
 	code = parse(code).replace('\r', '')
-	print(code)
+	with open('code.' + extension[language], 'w+') as f:
+		f.write(code)
+	f.close()
+	runCode(language)
 
 #gets all accepted submission for a problem and calls helper functions
 def getSubmissions(contest_id, problem_id, max_pages):
@@ -60,5 +77,5 @@ def getSubmissions(contest_id, problem_id, max_pages):
 			getCode(submission_url)
 			return
 		page += 1
-# login(username, password)
+login(username, password)
 getSubmissions(contest_id, problem_id, max_pages)
